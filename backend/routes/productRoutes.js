ac01a0b5597242ab88bf499d8b7e8dd4295dfdf6
Sourav Choudhury <saurabc987@gmail.com>
2023-08-18 20:22:24 +0530

    created asyncHandler and error handler middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,26 @@
 import express from 'express';
-import products from '../data/products.js';
+import asyncHandler from '../middleware/asyncHandler.js';
+import Product from '../models/productModel.js';
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json(products);
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
-router.get('/:id', (req, res) => {
-  const productId = req.params.id;
-  console.log('Requested product ID:', productId);
-
-  const product = products.find((product) => product._id === productId);
-  console.log('Found product:', product);
-
-  if (!product) {
-    return res.status(404).json({ message: 'Product not found' });
-  }
-
-  res.json(product);
-});
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    }
+    res.status(404);
+    throw new Error('Resource not found');
+  })
+);
 
 export default router;
